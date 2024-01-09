31632002bd673bdc7bcae412744cfb059ea218d2
snglAyush126 <snglayush@gmail.com>
2024-01-09 19:22:44 +0530

    Final submission

---
+++ fast-api-demo-app/main.py
@@ -0,0 +1,95 @@
+import string
+from typing import Union
+
+from datetime import date 
+
+from fastapi import FastAPI, Response
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pymongo
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+uri = "mongodb+srv://snglayush:ayUsh12345@cluster0.1hvi32m.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/usage/{month}/{year}")
+async def read_item(month: int, year:int, userToken: str | None = None):
+
+    myclient = pymongo.MongoClient("mongodb+srv://snglayush:ayUsh12345@cluster0.1hvi32m.mongodb.net/?retryWrites=true&w=majority")
+    mydb = myclient["mydatabase"]
+    mydownloadcol = mydb["downloads"]
+    mychatcol = mydb["chats"]
+
+    downloadDatas = []
+    chatDatas = []
+
+    for downloadData in mydownloadcol.find({"month" : month, "year" : year, "userToken" : userToken}):
+        print(downloadData)
+        downloadDatas.append({"day":downloadData["day"],"month":downloadData["month"], "year":downloadData["year"], "data_id":downloadData["data_id"]})
+    for chatData in mychatcol.find({"month" : month, "year" : year, "userToken" : userToken}):
+        print(chatData)
+        chatDatas.append({"day":chatData["day"],"month" : chatData["month"], "year" : chatData["year"], "chat_id" : chatData["chat_id"]})
+    
+    return {"downloadDatas" : downloadDatas, "chatDatas" : chatDatas}
+
+
+@app.get("/download/{data_id}")
+async def read_item(data_id: str, userToken: str | None = None):
+
+  
+    todays_date = date.today() 
+
+
+    myclient = pymongo.MongoClient("mongodb+srv://snglayush:ayUsh12345@cluster0.1hvi32m.mongodb.net/?retryWrites=true&w=majority")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["downloads"]
+
+    mydict = {"year":todays_date.year, "month":todays_date.month, "day":todays_date.day,"data_id":data_id, "userToken":userToken}
+
+    x = mycol.insert_one(mydict)
+    
+    return {"userToken":userToken}
+
+@app.get("/chat/{chat_id}")
+async def read_item(chat_id: int, userToken: str | None = None):
+
+    myclient = pymongo.MongoClient("mongodb+srv://snglayush:ayUsh12345@cluster0.1hvi32m.mongodb.net/?retryWrites=true&w=majority")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["chats"]
+
+    todays_date = date.today() 
+
+    mydict = {"year":todays_date.year, "month":todays_date.month, "day":todays_date.day,"chat_id": chat_id, "userToken":userToken}
+
+    x = mycol.insert_one(mydict)
+
+    return {"userToken":userToken}
